742e1581c465207659ce28da2a84d76ed31fb66f
Philipp Stuerner <philipp.stuerner@web.de>
2023-01-20 12:02:39 +0100

    add alert if no data found

--- services/app/static/main.js
+++ services/app/static/main.js
@@ -7,6 +7,11 @@ d3.select("#ticker").on("change", function () {
     var ticker = d3.select(this).property("value")
 
     d3.json(`http://localhost:8001/data?symbol=${ticker}`).then(function (d) {
+        if (d.data.length == 0) {
+            alert(`No data found for ${d.symbol} 😢.`)
+            return
+        }
+
         var chart = lineChart().height(250).width(width).symbol(d.symbol).data(d.data)
 
         chartDiv.call(chart);
